a14dda2ec762c2d9d95250900b83c2d864f2e2c7
Yacine Tazdait <yacine.tazdait@sogeti.com>
2023-02-06 17:00:38 +0100

    ReverseArrayChallenge: terminé

--- Challenges/MnimumElementChallenge/src/Main.java
+++ Challenges/MnimumElementChallenge/src/Main.java
@@ -7,6 +7,13 @@ public class Main {
         System.out.println(Arrays.toString(res));
         int val = Main.findMin(res);
         System.out.println("The minimum value is : " + val);
+        System.out.println("-".repeat(20));
+
+        System.out.println("Array before it has been reversed");
+        System.out.println(Arrays.toString(res));
+        Main.reverseArray(res);
+        System.out.println("Array after it has been reversed");
+        System.out.println(Arrays.toString(res));
     }
 
     private static int[] readIntegers(){
@@ -23,5 +30,15 @@ public class Main {
                 min = elt;
         }
         return min;
+    }
+
+    private static void reverseArray(int[] array){
+        int j = array.length -1, tmp;
+        for (int i = 0; i <= j ; i++) {
+            tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+            j--;
+        }
     }
 }
